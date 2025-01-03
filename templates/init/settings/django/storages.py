2aafe7ff48b6ee44428e092667fe963da8c97619
Siavash Outadi <siavash.outadi@gmail.com>
2025-01-03 18:52:15 +0100

    Add whitenoise package

---
+++ templates/init/settings/django/storages.py
@@ -0,0 +1,8 @@
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
