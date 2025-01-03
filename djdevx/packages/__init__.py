d5e20f36d885fd79c9674d5498e3a30ebd7ce336
Siavash Outadi <siavash.outadi@gmail.com>
2025-01-03 18:52:15 +0100

    Add whitenoise package

--- djdevx/packages/__init__.py
+++ djdevx/packages/__init__.py
@@ -1,6 +1,7 @@
 import typer
 
 from .django_browser_reload import app as browser_reload
+from .whitenoise import app as whitenoise
 
 app = typer.Typer(no_args_is_help=True)
 
@@ -8,4 +9,9 @@ app.add_typer(
     browser_reload,
     name="django-browser-reload",
     help="Manage django-browser-reload package",
+)
+app.add_typer(
+    whitenoise,
+    name="whitenoise",
+    help="Manage whitenoise package",
 )
