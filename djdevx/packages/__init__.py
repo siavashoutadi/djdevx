bffe80606c38ac16952efaeb74db19ce6a39567a
Siavash Outadi <siavash.outadi@gmail.com>
2025-01-11 13:57:20 +0100

    Add django-debug-toolbar package

--- djdevx/packages/__init__.py
+++ djdevx/packages/__init__.py
@@ -1,17 +1,24 @@
 import typer
 
-from .django_browser_reload import app as browser_reload
 from .whitenoise import app as whitenoise
+from .django_browser_reload import app as browser_reload
+from .django_debug_toolbar import app as debug_toolbar
+
 
 app = typer.Typer(no_args_is_help=True)
 
+app.add_typer(
+    whitenoise,
+    name="whitenoise",
+    help="Manage whitenoise package",
+)
 app.add_typer(
     browser_reload,
     name="django-browser-reload",
     help="Manage django-browser-reload package",
 )
 app.add_typer(
-    whitenoise,
-    name="whitenoise",
-    help="Manage whitenoise package",
+    debug_toolbar,
+    name="django-debug-toolbar",
+    help="Manage django-debug-toolbar package",
 )
