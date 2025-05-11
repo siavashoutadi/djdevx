5e85a719def2d402ca38cb43af55bf7395c9c72d
Siavash Outadi <siavash.outadi@gmail.com>
2025-05-11 13:17:58 +0200

    Add django-permissions-policy package

---
+++ djdevx/packages/django_permissions_policy.py
@@ -0,0 +1,57 @@
+import subprocess
+import typer
+
+from pathlib import Path
+
+from ..utils.print_console import print_step, print_success
+from ..utils.project_info import has_dependency
+from ..utils.project_files import (
+    copy_template_files,
+    is_project_exists_or_raise,
+    get_project_path,
+    get_packages_url_path,
+    get_packages_settings_path,
+)
+
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def install():
+    """
+    Install and configure django-permissions-policy
+    """
+    is_project_exists_or_raise()
+
+    print_step("Installing django-permissions-policy package ...")
+    subprocess.check_call(["uv", "add", "django-permissions-policy"])
+
+    current_dir = Path(__file__).resolve().parent
+    source_dir = current_dir.parent.parent / "templates" / "django-permissions-policy"
+    project_dir = get_project_path()
+
+    copy_template_files(
+        source_dir=source_dir, dest_dir=project_dir, template_context={}
+    )
+
+    print_success("django-permissions-policy is installed successfully.")
+
+
+@app.command()
+def remove():
+    """
+    Remove django-permissions-policy package
+    """
+    print_step("Removing django-permissions-policy package ...")
+    if has_dependency("django-permissions-policy"):
+        subprocess.check_call(["uv", "remove", "django-permissions-policy"])
+
+    url_path = Path.joinpath(get_packages_url_path(), "django_permissions_policy.py")
+    url_path.unlink(missing_ok=True)
+
+    settings_url = Path.joinpath(
+        get_packages_settings_path(), "django_permissions_policy.py"
+    )
+    settings_url.unlink(missing_ok=True)
+
+    print_success("django-permissions-policy is removed successfully.")
---
+++ templates/django-permissions-policy/settings/packages/django_permissions_policy.py
@@ -0,0 +1,27 @@
+from settings import MIDDLEWARE
+from typing import Dict, List
+
+permission_policy_middleware = "django_permissions_policy.PermissionsPolicyMiddleware"
+
+if permission_policy_middleware not in MIDDLEWARE:
+    security_index = MIDDLEWARE.index("django.middleware.security.SecurityMiddleware")
+    MIDDLEWARE.insert(security_index + 1, permission_policy_middleware)
+
+
+PERMISSIONS_POLICY: Dict[str, List[str]] = {
+    "accelerometer": [],
+    "ambient-light-sensor": [],
+    "autoplay": [],
+    "camera": [],
+    "display-capture": [],
+    "encrypted-media": [],
+    "fullscreen": [],
+    "geolocation": [],
+    "gyroscope": [],
+    "interest-cohort": [],
+    "magnetometer": [],
+    "microphone": [],
+    "midi": [],
+    "payment": [],
+    "usb": [],
+}
