ba598d482d4642cb9ee8cd7eb7f1024c611caece
Siavash Outadi <siavash.outadi@gmail.com>
2025-07-02 23:12:44 +0200

    Only add django_browser_reload and django_debug_toolbar in dev
    Since the dependencies are installed only in dev

--- djdevx/templates/django_browser_reload/settings/packages/django_browser_reload.py
+++ djdevx/templates/django_browser_reload/settings/packages/django_browser_reload.py
@@ -1,9 +1,11 @@
-from settings import INSTALLED_APPS, MIDDLEWARE
+from settings.django.base import INSTALLED_APPS, MIDDLEWARE, DEBUG
 
-INSTALLED_APPS += [
-    "django_browser_reload",
-]
 
-MIDDLEWARE += [
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
-]
+if DEBUG:
+    INSTALLED_APPS += [
+        "django_browser_reload",
+    ]
+
+    MIDDLEWARE += [
+        "django_browser_reload.middleware.BrowserReloadMiddleware",
+    ]
