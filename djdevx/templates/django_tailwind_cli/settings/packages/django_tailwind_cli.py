d19f33945e1cad3fb638ace3f469274e7e715346
Siavash Outadi <siavash.outadi@gmail.com>
2025-07-14 19:26:48 +0200

    Change tailwind-cli configuration
    - Move input.css for tailwind outside static.
      Whitenoise is loading all files in static folder
      and then it failed to load the @tailwindcss since
      the file was not in the same directory. The input.css
      is not used in production so we can safely move it
      outside the static directory.
    
    - Pin the tailwind cli to a specific version to make
      it more predictable and stable
    
    - Configure the dist css file name.

--- djdevx/templates/django_tailwind_cli/settings/packages/django_tailwind_cli.py
+++ djdevx/templates/django_tailwind_cli/settings/packages/django_tailwind_cli.py
@@ -5,4 +5,6 @@ INSTALLED_APPS += [
     "django_tailwind_cli",
 ]
 
-TAILWIND_CLI_SRC_CSS = BASE_DIR / "static" / "src" / "css" / "input.css"
+TAILWIND_CLI_VERSION = "4.1.3"
+TAILWIND_CLI_SRC_CSS = BASE_DIR / "tailwind" / "src" / "css" / "input.css"
+TAILWIND_CLI_DIST_CSS = "css/tailwind.min.css"
