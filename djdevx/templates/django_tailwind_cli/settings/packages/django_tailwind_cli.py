6314f82d3508f00591c04d5d1ba0a4fc934662f7
Siavash Outadi <siavash.outadi@gmail.com>
2025-10-28 19:35:20 +0100

    Do not hardcode the tailwind cli version

--- djdevx/templates/django_tailwind_cli/settings/packages/django_tailwind_cli.py
+++ djdevx/templates/django_tailwind_cli/settings/packages/django_tailwind_cli.py
@@ -5,6 +5,5 @@ INSTALLED_APPS += [
     "django_tailwind_cli",
 ]
 
-TAILWIND_CLI_VERSION = "4.1.3"
 TAILWIND_CLI_SRC_CSS = BASE_DIR / "tailwind" / "src" / "css" / "input.css"
 TAILWIND_CLI_DIST_CSS = "css/tailwind.min.css"
